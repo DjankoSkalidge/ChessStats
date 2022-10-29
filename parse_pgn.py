f81da63d01605a96f89e0785d14bd0f3c3ebc3a4
jaco <jaco@mediadistillery.com>
2022-10-29 21:24:27 +0200

    parse into csv & cleanup of grammar

--- parse_pgn.py
+++ parse_pgn.py
@@ -1,14 +1,40 @@
 import argparse
 import json
 from pgn_grammar import file
+import pandas as pd
 
+
+def to_csv(parsedoutput: dict) -> pd.DataFrame:
+    columns = ['event', 'white', 'black', 'time_control', 'datetime', 'moves', 'result', 'termination', 'white_rating', 'black_rating', 'opening']
+    df = pd.DataFrame(columns=columns)
+    for game in parsedoutput:
+        df = pd.concat([df, pd.DataFrame([{'event': game["annotations"].get("Event", "unknown"),
+                         'white': game["annotations"].get("White", "unknown"),
+                         'black': game["annotations"].get("Black", "unknown"),
+                         'time_control': game["annotations"].get("TimeControl", "unknown"),
+                         'datetime': f"{game['annotations'].get('UTCDate')} {game['annotations'].get('UTCTime')}",
+                         'moves': json.dumps(game["game"]["moves"]),
+                         'result': game["game"]["outcome"],
+                         'termination': game["annotations"].get("Termination", "unknown"),
+                         'white_rating': game["annotations"].get("WhiteElo", "unknown"),
+                         'black_rating': game["annotations"].get("BlackElo", "unknown"),
+                         'opening': game["annotations"].get("Opening", "unknown")
+                         }])],axis=0, ignore_index=True)
+    return df
 
 def main():
     args = parse_args()
+    if args.output_format == 'mp4':
+        print("Wat dacht jezelf?")
+        exit()
     with open(args.input, 'r') as f:
         parsedoutput = file.parse(f.read()).or_die()
-    with open(args.output, 'w+') as f:
-        json.dump(parsedoutput, f)
+    if args.output_format == 'json':
+        with open(args.output, 'w+') as f:
+            json.dump(parsedoutput, f)
+    elif args.output_format == 'csv':
+        to_csv(parsedoutput).to_csv(args.output)
+
 
 
 def parse_args():
@@ -16,6 +42,7 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('input', help='Path of pgn file', type=str)
     parser.add_argument('output', help='Destination of parsed output', type=str)
+    parser.add_argument('--output-format', choices=['csv', 'json', 'mp4'], type=str, default='json')
 
     return parser.parse_args()
 
--- pgn_grammar.py
+++ pgn_grammar.py
@@ -1,11 +1,14 @@
 from parsita import *
 from parsita.util import constant
 
+
 def formatannotations(annotations):
     return {ant[0]: ant[1] for ant in annotations}
 
+
 def formatmoveannotations(moveannotations):
     return {ant[0]: ant[1] for ant in moveannotations}
+
 
 def formatgame(game):
     return {
@@ -13,11 +16,20 @@ def formatgame(game):
         'outcome': game[1]
     }
 
+
 def formatmove(move):
     return {
         'move': move[0],
-        'annotations': move[1] if len(move) > 0 else None
+        'annotations': move[1] if len(move) > 0 else {}
     }
+
+
+def formatsimplemove(move):
+    return {
+        'move': move,
+        'annotations': {}
+    }
+
 
 def formatturn(turn):
     return {
@@ -29,8 +41,10 @@ def formatturn(turn):
         'blackmove': turn[2]
     }
 
+
 def formatentry(entry):
     return {'annotations': entry[0], 'game': entry[1]}
+
 
 def handleoptional(optionalmove):
     if len(optionalmove) > 0:
@@ -38,40 +52,42 @@ def handleoptional(optionalmove):
     else:
         return None
 
+whitespace = lit(' ') | lit('\n') | lit('\t')
 
-quote = lit(r'"')
-whitespace = lit(' ') | lit('\n')
-tag = reg(r'[\u0021-\u0021\u0023-\u005A\u005E-\u007E]+')
-string = reg(r'[\u0020-\u0021\u0023-\u005A\u005E-\U0010FFFF]+')
+quotation_mark = lit(r'"')
+annotationtag = reg(r'[\u0021-\u0021\u0023-\u005A\u005E-\u007E]+')
+annotationvalue = reg(r'[\u0020-\u0021\u0023-\u005A\u005E-\U0010FFFF]+')
 
-annotation = '[' >> (tag) << ' ' & (quote >> string << quote) << ']'
+annotation = '[' >> annotationtag << ' ' & (quotation_mark >> annotationvalue << quotation_mark) << ']'
 annotations = repsep(annotation, '\n') > formatannotations
 
-regularmove = reg(r'[a-h1-8NBRQKx\+#=]+') # Matches more than just chess moves
-longcastle = reg(r'O-O-O[+#]?') # match first to avoid castle matching spuriously
+regularmove = reg(r'[a-h1-8NBRQKx\+#=]+')
+longcastle = reg(r'O-O-O[+#]?')
 castle = reg(r'O-O[+#]?')
-nullmove = lit('--') # Illegal move rarely used in annotations
-chessmove = regularmove | longcastle | castle | nullmove
+nullmove = lit('--')
+chessmove = regularmove | longcastle | castle | nullmove  # Order is important, longcastle needs to be tried before castle
 
 whitemovenumber = (reg(r'[0-9]+') << '.' << whitespace) > int
-blackmovenumber = (reg(r'[0-9]+') << '...' << whitespace) > int
+blackmovenumber = (reg(r'[0-9]+') << '...' << whitespace)
 
-simpleturn = whitemovenumber & (chessmove << whitespace) > formatmove & (opt(chessmove << whitespace) > handleoptional) > formatturn
+simplemove = chessmove > formatsimplemove
+simpleturn = whitemovenumber & (simplemove << whitespace) & (opt(chessmove << whitespace) > handleoptional) > formatturn
 
 moveannotationtag = reg(r'[\u0025][\u0021-\u0021\u0023-\u005A\u005E-\u007E]+')
 moveannotationvalue = reg(r'[\u0021-\u0021\u0023-\u005A\u005E-\u007E]+')
-moveannotation = '[' >> (moveannotationtag) << ' ' & (moveannotationvalue) << ']'
+moveannotation = '[' >> moveannotationtag << ' ' & moveannotationvalue << ']'
 moveannotations = '{ ' >> repsep(moveannotation, ' ') << ' }' > formatmoveannotations
 
 move = chessmove << ' ' & (opt(moveannotations) > handleoptional) > formatmove
-turn = whitemovenumber & (move << whitespace) & (opt(blackmovenumber >> move << whitespace) > handleoptional) > formatturn
+turn = whitemovenumber & (move << whitespace) & (
+            opt(blackmovenumber >> move << whitespace) > handleoptional) > formatturn
 
 draw = lit('1/2-1/2')
 white = lit('1-0')
 black = lit('0-1')
 outcome = draw | white | black
 
-game = (rep(turn|simpleturn) & outcome) > formatgame
+game = (rep(turn | simpleturn) & outcome) > formatgame
 
 entry = ((annotations << rep(whitespace)) & (game << rep(whitespace))) > formatentry
 
