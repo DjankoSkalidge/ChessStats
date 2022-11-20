3ba51058cfa4e4daf81328ad2d46861bb9b9f32b
jaco <jaco@mediadistillery.com>
2022-11-20 19:06:30 +0100

    Opening Detector Notebook
    Grammar for parsing openings into movelists. Notebook describing how to get and search in an opening graph structure.

--- src/jchess/pgn/parser.py
+++ src/jchess/pgn/parser.py
@@ -16,6 +16,15 @@ def formatgame(game):
     }
 
 
+def formatopening(opening):
+    list_of_moves = []
+    for moves in opening:
+        for move in moves:
+            if move is not None:
+                list_of_moves.append(move)
+    return list_of_moves
+
+
 def formatmove(move):
     return {
         'notation': move[0],
@@ -51,6 +60,7 @@ def handleoptional(optionalmove):
     else:
         return None
 
+
 whitespace = lit(' ') | lit('\n') | lit('\t')
 
 quotation_mark = lit(r'"')
@@ -70,7 +80,8 @@ whitemovenumber = (reg(r'[0-9]+') << '.' << whitespace) > int
 blackmovenumber = (reg(r'[0-9]+') << '...' << whitespace)
 
 simplemove = chessmove > formatsimplemove
-simpleturn = whitemovenumber & (simplemove << whitespace) & (opt(simplemove << whitespace) > handleoptional) > formatturn
+simpleturn = whitemovenumber & (simplemove << whitespace) & (
+            opt(simplemove << whitespace) > handleoptional) > formatturn
 
 moveannotationtag = reg(r'[\u0025][\u0021-\u0021\u0023-\u005A\u005E-\u007E]+')
 moveannotationvalue = reg(r'[\u0021-\u0021\u0023-\u005A\u005E-\u007E]+')
@@ -79,7 +90,7 @@ moveannotations = '{ ' >> repsep(moveannotation, ' ') << ' }' > formatmoveannota
 
 move = chessmove << ' ' & (opt(moveannotations) > handleoptional) > formatmove
 turn = whitemovenumber & (move << whitespace) & (
-            opt(blackmovenumber >> move << whitespace) > handleoptional) > formatturn
+        opt(blackmovenumber >> move << whitespace) > handleoptional) > formatturn
 
 draw = lit('1/2-1/2')
 white = lit('1-0')
@@ -87,6 +98,10 @@ black = lit('0-1')
 outcome = draw | white | black
 
 game = (rep(turn | simpleturn) & outcome) > formatgame
+
+# Only applicable for the opening database parser
+openingmove = chessmove << opt(whitespace)
+openingnotation = (rep(whitemovenumber >> openingmove & (opt(openingmove) > handleoptional))) > formatopening
 
 entry = ((annotations << rep(whitespace)) & (game << rep(whitespace))) > formatentry
 
